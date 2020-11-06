1923419ef714903619e5b40fc17ebfbbbc7522e1
unknown <nguyenkhanh02092000@gmail.com>
2020-11-06 17:14:18 +0700

    update

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb://localhost:27017/shop',
+        dbname : "shop",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://khanh123456:pBkHbPIb4bx1rKuZ@cluster0.ayy8a.mongodb.net/shop?retryWrites=true&w=majority',
+        dbname : "shop",
+        dbusername : "khanh123456",
+        dbpassword : "pBkHbPIb4bx1rKuZ",
+    }
+
+};
